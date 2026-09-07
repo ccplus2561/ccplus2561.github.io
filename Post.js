712f182779cd22a308bd49ccb491c9d34384120c
ccplus2561 <ccplus2561@gmail.com>
2026-09-07 14:33:50 +0800

    Post.js

--- Post.js
+++ Post.js
@@ -1,30 +1,8 @@
 // 在 Google Apps Script 編輯器裡建立一個名為 "Post" 的 HTML 檔案
 var postData = [
   {
-    version: "【講義】v26.0909",
-    isExpanded: true,
-    logs: [
-      {
-        tag: "上架",
-        title: "[11] ASRPRO 語音模組",
-        content: "發布 ex01~ex10 篇教學。"
-      }
-    ]
-  },
-{
-    version: "【更新】v26.0908",
-    isExpanded: false,
-    logs: [
-      {
-        tag: "修正",
-        title: "網頁架構",
-        content: "加強系統安全防護並優化架構，網頁運作更流暢。"
-      }
-    ]
-  },
-  {
     version: "【講義】v26.0907",
-    isExpanded: false,
+    isExpanded: true,
     logs: [
       {
         tag: "上架",
